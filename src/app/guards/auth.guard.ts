d14f3ed5749be89f83210797a3a7f163d3481629
evan <evan.dev127@gmail.com>
2024-02-05 10:41:28 +0330

    update v1.02 Responsive Home Page

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject, Injectable} from "@angular/core";
+import {AuthService} from "../services/auth.service";
+import {NgToastService} from "ng-angular-popup";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+class AuthGuard{
+  constructor(private auth : AuthService, private router : Router, private toast : NgToastService) {
+
+  }
+  canActivate(): boolean {
+    if (this.auth.isLoggedIn()){
+      return true;
+    } else {
+      this.toast.error({detail:"ERROR" , summary:"Please Login First!"});
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+}
+
+export const authGuard: CanActivateFn = ():boolean => {
+  return inject(AuthGuard).canActivate();
+};
