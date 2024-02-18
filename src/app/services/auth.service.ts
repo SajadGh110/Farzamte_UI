3249ff87bcac5abf897b02e2ff6640aaed8d3e6e
evan <evan.dev127@gmail.com>
2024-02-18 16:25:29 +0330

    update v1.06 path update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,8 +6,7 @@ import {Router} from "@angular/router";
   providedIn: 'root'
 })
 export class AuthService {
-
-  private baseUrl:string = "https://localhost:44397/api/Users/"
+  private baseUrl:string = "http://192.168.233.4:8080/api/Users/"
   constructor(private http : HttpClient, private router : Router) { }
 
   register(userObj:any){
