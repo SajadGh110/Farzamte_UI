948009a4d1ad2007de99d3e8db835cb6cfbd78c0
evan <evan.dev127@gmail.com>
2024-02-20 13:33:05 +0330

    update v1.1 - New Pages - New Routes - All Styles Splitted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
+import { IndexComponent } from './components/index/index.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {authGuard} from "./guards/auth.guard";
+import {HomeComponent} from "./components/home/home.component";
+import {SupportComponent} from "./components/support/support.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ReportComponent} from "./components/report/report.component";
+import {CrmComponent} from "./components/crm/crm.component";
+import {CallcenterComponent} from "./components/callcenter/callcenter.component";
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
+  {path:'', component: IndexComponent},
+  {path:'home', component: HomeComponent},
   {path:'login', component: LoginComponent},
+  {path:'support', component: SupportComponent},
+  {path:'about', component: AboutComponent},
+  {path:'report', component: ReportComponent},
+  {path:'crm', component: CrmComponent},
+  {path:'callcenter', component: CallcenterComponent},
   {path:'register', component: RegisterComponent},
   {path:'dashboard', component: DashboardComponent,canActivate:[authGuard]}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
+import { IndexComponent } from './components/index/index.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgToastModule } from 'ng-angular-popup';
+import { AboutComponent } from './components/about/about.component';
+import { SupportComponent } from './components/support/support.component';
+import { ReportComponent } from './components/report/report.component';
+import { CrmComponent } from './components/crm/crm.component';
+import { CallcenterComponent } from './components/callcenter/callcenter.component';
+import { HomeComponent } from './components/home/home.component';
+import { Footer1Component } from './components/Template/footer1/footer1.component';
+import { Menu1Component } from './components/Template/menu1/menu1.component';
+import { Menu2Component } from './components/Template/menu2/menu2.component';
+import { Footer2Component } from './components/Template/footer2/footer2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
+    IndexComponent,
+    DashboardComponent,
+    AboutComponent,
+    SupportComponent,
+    ReportComponent,
+    CrmComponent,
+    CallcenterComponent,
     HomeComponent,
-    DashboardComponent
+    Footer1Component,
+    Menu1Component,
+    Menu2Component,
+    Footer2Component
   ],
   imports: [
     BrowserModule,
