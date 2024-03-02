37eac0421769fe90d98ab7dd09b6c681881ed523
evan <evan.dev127@gmail.com>
2024-03-02 20:10:36 +0330

    update v1.8 - improved css

--- src/app/components/Template/footer1/footer1.component.ts
+++ src/app/components/Template/footer1/footer1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-footer1',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer1.component.scss']
 })
 export class Footer1Component {
-
+  @Input() img: boolean = false;
 }
