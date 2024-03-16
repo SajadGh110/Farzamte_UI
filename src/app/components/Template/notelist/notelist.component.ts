aa20508f8f6aebf0a1d5b21a478735644f4e30a6
evan <evan.dev127@gmail.com>
2024-03-16 03:51:58 +0330

    update v1.98 - callcenter page responsive need rework at notelist

--- src/app/components/Template/notelist/notelist.component.ts
+++ src/app/components/Template/notelist/notelist.component.ts
@@ -9,4 +9,6 @@ export class NotelistComponent {
   @Input() title!: string;
   @Input() items!: string[];
   @Input() color!: string;
+  @Input() bs_width!: string;
+  @Input() bs_width_reverse!: string;
 }
