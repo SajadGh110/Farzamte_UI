df63b92ad52d503d6e67ad9391fe85a6f6f76a2b
evan <evan.dev127@gmail.com>
2024-07-20 01:31:23 +0330

    update v3.01 - HappyCall Improved

--- src/app/components/panel/happy-call/happy-call.component.ts
+++ src/app/components/panel/happy-call/happy-call.component.ts
@@ -4,7 +4,7 @@ import {DashboardSidebarComponent} from "../../Template/dashboard-sidebar/dashbo
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {NgToastService} from "ng-angular-popup";
 import {format, subDays} from "date-fns";
-import {BarChartModule, LineChartModule, ScaleType} from "@swimlane/ngx-charts";
+import {BarChartModule, LineChartModule, NumberCardModule, ScaleType} from "@swimlane/ngx-charts";
 import {NgForOf, NgIf} from "@angular/common";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {HappycallService} from "../../../services/happycall.service";
@@ -20,25 +20,35 @@ import {HappycallService} from "../../../services/happycall.service";
     BarChartModule,
     NgIf,
     LineChartModule,
-    NgForOf
+    NgForOf,
+    NumberCardModule
   ],
   templateUrl: './happy-call.component.html',
   styleUrl: './happy-call.component.scss'
 })
 export class HappyCallComponent implements OnInit {
   protected readonly ScaleType = ScaleType;
-  protected readonly customcolor = [{name: 'کل تماس ها', value: '#9cfcff'},{name: 'تماس های موفق', value: '#69ff79'},{name: 'تماس های ناموفق', value: '#ff8181'}];
+  protected readonly linecharts_colors = [{name: 'کل تماس ها', value: '#9cfcff'},{name: 'تماس های موفق', value: '#69ff79'},{name: 'تماس های ناموفق', value: '#ff8181'}];
+  protected readonly series_nubmercards_labels = ['تعداد مشتریان','مشتریان فعال','مشتریان غیرفعال','تماس موفق','فعال شدن پس از هپی کال','فعالیت در سایر کارگزاری ها','معرفی باشگاه مشتریان']
+  protected readonly numbercards_colors = [{name:this.series_nubmercards_labels[0],value:'#8cff7f'},{name:this.series_nubmercards_labels[1],value:'#66d9ff'},{name:this.series_nubmercards_labels[2],value: '#ff7c5a'},{name:this.series_nubmercards_labels[3],value: '#d586ff'},{name:this.series_nubmercards_labels[4],value: '#ffd56d'},{name:this.series_nubmercards_labels[5],value: '#adadad'},{name:this.series_nubmercards_labels[6],value: '#93fff4'}];
   private currentDate:Date = new Date();
   dateform! : FormGroup;
   protected flag_count:boolean=false;
-  protected flag_data:boolean=false;
+  protected flag_g1:boolean=false;
   public constructor(private toast:NgToastService, private fb:FormBuilder, private getData:HappycallService) {}
   //StartDate:string = format(subDays(this.currentDate, 30), 'yyyy-MM-dd');
   //EndDate:string = format(this.currentDate, 'yyyy-MM-dd');
   StartDate:string = "2024-04-01";
   EndDate:string = "2024-06-01";
   series_happycalls: any[] = [{ name: "کل تماس ها", series: [] }, { name: "تماس های موفق", series: [] }, { name: "تماس های ناموفق", series: [] }];
-  data: any[] = [];
+  series_numbercards: any[] = [];
+  Customers_Count:Number = 0;
+  Active_Customers_Count:Number = 0;
+  Inactive_Customers_Count:Number = 0;
+  SuccessfulCalls_Count:Number = 0;
+  ActiveAfterCalls_Count:Number = 0;
+  ActiveInOtherBrockers_Count:Number = 0;
+  ExplanationClub_Count:Number = 0;
   async ngOnInit(){
     this.dateform = this.fb.group({
       StartDate: [''],
@@ -54,7 +64,7 @@ export class HappyCallComponent implements OnInit {
     this.series_happycalls = [{ name: "کل تماس ها", series: [] }, { name: "تماس های موفق", series: [] }, { name: "تماس های ناموفق", series: [] }];
     try {
       // ------------------------------------
-      let res1 = await this.getData.get_Total_Count(stDate,enDate).toPromise();
+      let res1 = await this.getData.get_Total_Count_Day(stDate,enDate).toPromise();
       this.series_happycalls[0].name = "کل تماس ها";
       let series_Total: any[] = [];
       for (let i = 0; i < res1.length; i++) {
@@ -62,7 +72,7 @@ export class HappyCallComponent implements OnInit {
       }
       this.series_happycalls[0].series = series_Total;
       // ------------------------------------
-      let res2 = await this.getData.get_SuccessfulCalls_Count(stDate,enDate).toPromise();
+      let res2 = await this.getData.get_SuccessfulCalls_Count_Day(stDate,enDate).toPromise();
       this.series_happycalls[1].name = "تماس های موفق";
       let series_SuccessfulCalls: any[] = [];
       for (let i = 0; i < res2.length; i++) {
@@ -70,7 +80,7 @@ export class HappyCallComponent implements OnInit {
       }
       this.series_happycalls[1].series = series_SuccessfulCalls;
       // ------------------------------------
-      let res3 = await this.getData.get_UnsuccessfulCalls_Count(stDate,enDate).toPromise();
+      let res3 = await this.getData.get_UnsuccessfulCalls_Count_Day(stDate,enDate).toPromise();
       this.series_happycalls[2].name = "تماس های ناموفق";
       let series_UnsuccessfulCalls: any[] = [];
       for (let i = 0; i < res3.length; i++) {
@@ -78,11 +88,46 @@ export class HappyCallComponent implements OnInit {
       }
       this.series_happycalls[2].series = series_UnsuccessfulCalls;
       this.flag_count = true;
+      // ------------------------------------
+      this.Customers_Count = await this.getData.get_Customers_Count(stDate,enDate).toPromise();
+      this.Active_Customers_Count = await this.getData.get_Active_Customers_Count(stDate,enDate).toPromise();
+      this.Inactive_Customers_Count = await this.getData.get_Inactive_Customers_Count(stDate,enDate).toPromise();
+      this.SuccessfulCalls_Count = await this.getData.get_SuccessfulCalls_Count(stDate,enDate).toPromise();
+      this.ActiveAfterCalls_Count = await this.getData.get_ActiveAfterCalls_Count(stDate,enDate).toPromise();
+      this.ActiveInOtherBrockers_Count = await this.getData.get_ActiveInOtherBrockers_Count(stDate,enDate).toPromise();
+      this.ExplanationClub_Count = await this.getData.get_ExplanationClub_Count(stDate,enDate).toPromise();
+      this.series_numbercards = [
+        {
+          "name": this.series_nubmercards_labels[0],
+          "value": this.Customers_Count
+        },
+        {
+          "name": this.series_nubmercards_labels[1],
+          "value": this.Active_Customers_Count
+        },
+        {
+          "name": this.series_nubmercards_labels[2],
+          "value": this.Inactive_Customers_Count
+        },
+        {
+          "name": this.series_nubmercards_labels[3],
+          "value": this.SuccessfulCalls_Count
+        },
+        {
+          "name": this.series_nubmercards_labels[4],
+          "value": this.ActiveAfterCalls_Count
+        },
+        {
+          "name": this.series_nubmercards_labels[5],
+          "value": this.ActiveInOtherBrockers_Count
+        },
+        {
+          "name": this.series_nubmercards_labels[6],
+          "value": this.ExplanationClub_Count
+        }
+        ]
 
-      let res4 = await this.getData.get_All(stDate,enDate).toPromise();
-      this.data = res4;
-      this.flag_data = true;
-
+      this.flag_g1 = true;
     }catch (error:any){
       this.toast.error({ detail: "ERROR", summary: error.message, duration: 5000, position: 'topRight' });
     }
--- src/app/services/happycall.service.ts
+++ src/app/services/happycall.service.ts
@@ -11,16 +11,48 @@ export class HappycallService {
   constructor(private http : HttpClient, private appConfigService: AppConfigService) { }
   header = new HttpHeaders().append("api-key",this.appConfigService.getApiKey()).append("Authorization",this.appConfigService.getToken());
 
-  get_Total_Count(StartDate:string,EndDate:string):Observable<any>{
-    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/Total_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  get_Customers_Count(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/Customers_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
   }
 
-  get_SuccessfulCalls_Count(StartDate:string,EndDate:string):Observable<any>{
+  get_UniqueCustomers(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/UniqueCustomers?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_Active_Customers_Count(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/Active_Customers_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_Inactive_Customers_Count(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/Inactive_Customers_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_SuccessfulCalls_Count(StartDate:string, EndDate:string):Observable<any>{
     return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/SuccessfulCalls_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
   }
 
-  get_UnsuccessfulCalls_Count(StartDate:string,EndDate:string):Observable<any>{
-    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/UnsuccessfulCalls_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  get_ActiveAfterCalls_Count(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/ActiveAfterCalls_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_ActiveInOtherBrockers_Count(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/ActiveInOtherBrockers_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_ExplanationClub_Count(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/ExplanationClub_Count?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_Total_Count_Day(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/Total_Count_Day?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_SuccessfulCalls_Count_Day(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/SuccessfulCalls_Count_Day?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
+  }
+
+  get_UnsuccessfulCalls_Count_Day(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}HappyCall/UnsuccessfulCalls_Count_Day?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
   }
 
   get_All(StartDate:string,EndDate:string):Observable<any>{
