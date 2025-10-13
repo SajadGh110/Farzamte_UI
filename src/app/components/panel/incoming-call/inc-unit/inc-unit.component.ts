da87c6bea8c71b258cf9242e26da16dbd75f02c6
Sajad Gholami <Evan.Dev127@gmail.com>
2025-10-13 16:35:21 +0330

    V7.1

--- src/app/components/panel/incoming-call/inc-unit/inc-unit.component.ts
+++ src/app/components/panel/incoming-call/inc-unit/inc-unit.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {DashboardContactComponent} from "../../../Template/dashboard-contact/dashboard-contact.component";
 import {DashboardSidebarComponent} from "../../../Template/dashboard-sidebar/dashboard-sidebar.component";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule, DatePipe, NgForOf, NgIf} from "@angular/common";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {NgToastService} from "ng-angular-popup";
@@ -15,6 +15,7 @@ import {MatSelectModule} from "@angular/material/select";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatInputModule} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-inc-unit',
@@ -32,7 +33,8 @@ import {MatButton} from "@angular/material/button";
     MatSelectModule,
     MatDatepickerModule,
     MatInputModule,
-    MatButton
+    MatButton,
+    MatSlideToggleModule
   ],
   providers: [DatePipe],
   templateUrl: './inc-unit.component.html',
@@ -41,12 +43,21 @@ import {MatButton} from "@angular/material/button";
 export class IncUnitComponent implements OnInit {
   dateform! : FormGroup;
   Units: string[] = [];
-  Types: string[] = ['تجمیع','مشتریان','سایرین'];
+  Types: string[] = ['تجمیع','مشتریان','سایرین','هیچکدام'];
   Branchs: string[] = [];
   selectedUnit: string = '';
   selectedType: string = this.Types[0];
   selectedBranch: string = '';
+  filterForm = new FormGroup({
+    flag_filter_type: new FormControl(false),
+    flag_filter_unit: new FormControl(false),
+    flag_filter_branch: new FormControl(false)
+  });
+  get flag_filter_type() { return this.filterForm.get('flag_filter_type')?.value; }
+  get flag_filter_unit() { return this.filterForm.get('flag_filter_unit')?.value; }
+  get flag_filter_branch() { return this.filterForm.get('flag_filter_branch')?.value; }
   protected flag_time:boolean = false;
+  protected flag_toggle_filter:boolean = false;
   protected flag_filter:boolean = false;
   protected flag_count:boolean=false;
   protected flag_Ph_Reasons_Totals:boolean=false;
@@ -67,15 +78,9 @@ export class IncUnitComponent implements OnInit {
   series_Popup_List: any[] = [];
   series_Phonecall_Reasons_Totals: any[] = [];
   series_color = ['#3ebeed','#EC7063','#004e75','#f1c40f','#7f6487','#42b3a1'];
-  TitleTextStyle: any= {
-    fontFamily: 'Nazanin', fontSize: '20px',
-  };
-  tooltipTextStyle: any = {
-    fontFamily: 'Nazanin', fontWeight: 'bold'
-  }
-  legendTextStyle: any = {
-    fontFamily: 'Nazanin', fontWeight: 'bold', fontSize:'14px'
-  }
+  TitleTextStyle: any= {fontFamily: 'Nazanin', fontSize: '20px',};
+  tooltipTextStyle: any = {fontFamily: 'Nazanin', fontWeight: 'bold'};
+  legendTextStyle: any = {fontFamily: 'Nazanin', fontWeight: 'bold', fontSize:'14px'};
   series_calls_count_day:EChartsOption = {
     title: {text: 'آمار کل تماس های ورودی - بر حسب روز', textStyle:this.TitleTextStyle,subtext:this.st_to_en, subtextStyle:{fontFamily:'Bahnschrift'}, right:0, textAlign:'center'},
     tooltip: {trigger: 'axis', axisPointer: {type: 'cross', label: {backgroundColor: '#6a7985'}},textStyle:this.tooltipTextStyle},
@@ -187,7 +192,7 @@ export class IncUnitComponent implements OnInit {
       await this.SetTime(30);
     }
   }
-  async GetData(stDate:string, enDate:string, selectedUnit:string, selectedType:string, selectedBranch:string):Promise<any> {
+  async GetData(stDate:string, enDate:string):Promise<any> {
     this.flag_count = false;
     this.flag_Ph_Reasons_Totals = false;
     this.flag_Reason_Detail_Totals = false;
@@ -196,7 +201,7 @@ export class IncUnitComponent implements OnInit {
     this.total_Reason_Detail_Totals = 0;
     this.series_Phonecall_Reasons_Totals = [];
     try {
-      let res1 = await this.getData.get_CountDay(stDate, enDate, selectedUnit, selectedType, selectedBranch).toPromise();
+      let res1 = await this.getData.get_CountDay(stDate, enDate).toPromise();
       let total_calls_date: any[] = [];
       let total_calls_count: any[] = [];
       for (let i = 0; i < res1.length; i++) {
@@ -211,11 +216,11 @@ export class IncUnitComponent implements OnInit {
       (this.series_calls_count_day.series as any)[0].data = total_calls_count;
       this.flag_count = true;
 
-      let res_top_reasons_Totals = await this.getData.get_TopReasons(stDate, enDate, selectedUnit, selectedType, selectedBranch).toPromise();
+      let res_top_reasons_Totals = await this.getData.get_TopReasons(stDate, enDate).toPromise();
       (this.series_Top_Reasons_bar_Totals.dataset as any).source = res_top_reasons_Totals;
       this.flag_Top_Reasons = true;
 
-      let res2_Totals = await this.getData.get_PhonecallReasons(stDate, enDate, selectedUnit, selectedType, selectedBranch).toPromise();
+      let res2_Totals = await this.getData.get_PhonecallReasons(stDate, enDate).toPromise();
       for (let i = 0; i < res2_Totals.length; i++){
         this.series_Phonecall_Reasons_Totals.push({ name: res2_Totals[i].reason, value: res2_Totals[i].count });
         this.total_Phonecall_Reasons_Totals += res2_Totals[i].count;
@@ -224,7 +229,60 @@ export class IncUnitComponent implements OnInit {
       (this.series_Phonecall_Reasons_tmp_Totals.series as any)[0].data = this.series_Phonecall_Reasons_Totals;
       this.flag_Ph_Reasons_Totals = true;
 
-      let res3_Totals = await this.getData.get_ReasonDetail(this.StartDate, this.EndDate, this.top_reason_Totals, selectedUnit, selectedType, selectedBranch).toPromise();
+      let res3_Totals = await this.getData.get_ReasonDetail(this.StartDate, this.EndDate, this.top_reason_Totals).toPromise();
+      let series_lbl_Totals: any[] = [];
+      let series_count_Totals: any[] = [];
+      for (let i = 0; i < res3_Totals.length; i++){
+        series_lbl_Totals.push(res3_Totals[i].reasonDetail);
+        series_count_Totals.push(res3_Totals[i].count);
+        this.total_Reason_Detail_Totals += res3_Totals[i].count;
+      }
+      this.reason_selected_Totals = this.top_reason_Totals;
+      (this.series_Reason_Detail_bar_Totals.yAxis as any)[0].data = series_lbl_Totals;
+      (this.series_Reason_Detail_bar_Totals.series as any)[0].data = series_count_Totals;
+      this.flag_Reason_Detail_Totals = true;
+    } catch (error:any) {
+      this.toast.error({detail: "ERROR", summary: error.message, duration: 5000, position: 'topRight'});
+    }
+  }
+  async GetData_F(stDate:string, enDate:string, selectedUnit:string, selectedType:string, selectedBranch:string):Promise<any> {
+    this.flag_count = false;
+    this.flag_Ph_Reasons_Totals = false;
+    this.flag_Reason_Detail_Totals = false;
+    this.flag_Top_Reasons = false;
+    this.total_Phonecall_Reasons_Totals = 0;
+    this.total_Reason_Detail_Totals = 0;
+    this.series_Phonecall_Reasons_Totals = [];
+    try {
+      let res1 = await this.getData.get_CountDay_F(stDate, enDate, selectedUnit, selectedType, selectedBranch).toPromise();
+      let total_calls_date: any[] = [];
+      let total_calls_count: any[] = [];
+      for (let i = 0; i < res1.length; i++) {
+        total_calls_date.push(res1[i].date);
+        total_calls_count.push(res1[i].count);
+      }
+      let std = this.datePipe.transform(total_calls_date[0],'yyyy-MM-dd') || '';
+      let end = this.datePipe.transform(total_calls_date[total_calls_date.length-1],'yyyy-MM-dd') || '';
+      this.st_to_en = std + ' to ' + end;
+      (this.series_calls_count_day.title as any).subtext = this.st_to_en;
+      (this.series_calls_count_day.xAxis as any).data = total_calls_date;
+      (this.series_calls_count_day.series as any)[0].data = total_calls_count;
+      this.flag_count = true;
+
+      let res_top_reasons_Totals = await this.getData.get_TopReasons_F(stDate, enDate, selectedUnit, selectedType, selectedBranch).toPromise();
+      (this.series_Top_Reasons_bar_Totals.dataset as any).source = res_top_reasons_Totals;
+      this.flag_Top_Reasons = true;
+
+      let res2_Totals = await this.getData.get_PhonecallReasons_F(stDate, enDate, selectedUnit, selectedType, selectedBranch).toPromise();
+      for (let i = 0; i < res2_Totals.length; i++){
+        this.series_Phonecall_Reasons_Totals.push({ name: res2_Totals[i].reason, value: res2_Totals[i].count });
+        this.total_Phonecall_Reasons_Totals += res2_Totals[i].count;
+      }
+      this.top_reason_Totals = this.series_Phonecall_Reasons_Totals[0].name;
+      (this.series_Phonecall_Reasons_tmp_Totals.series as any)[0].data = this.series_Phonecall_Reasons_Totals;
+      this.flag_Ph_Reasons_Totals = true;
+
+      let res3_Totals = await this.getData.get_ReasonDetail_F(this.StartDate, this.EndDate, this.top_reason_Totals, selectedUnit, selectedType, selectedBranch).toPromise();
       let series_lbl_Totals: any[] = [];
       let series_count_Totals: any[] = [];
       for (let i = 0; i < res3_Totals.length; i++){
@@ -245,7 +303,7 @@ export class IncUnitComponent implements OnInit {
     if (event.name){
       this.flag_Reason_Detail_Totals = false;
       this.total_Reason_Detail_Totals = 0;
-      let res = await this.getData.get_ReasonDetail(this.StartDate, this.EndDate, event.name, this.selectedUnit, this.selectedType, this.selectedBranch).toPromise();
+      let res = await this.getData.get_ReasonDetail_F(this.StartDate, this.EndDate, event.name, this.selectedUnit, this.selectedType, this.selectedBranch).toPromise();
       let series_lbl: any[] = [];
       let series_count: any[] = [];
       for (let i = 0; i < res.length; i++){
@@ -262,7 +320,7 @@ export class IncUnitComponent implements OnInit {
 
   async Popup_List_Totals(event:any){
     this.flag_loading = true;
-    this.series_Popup_List = await this.getData.get_Description(this.StartDate, this.EndDate, event.name, this.selectedUnit, this.selectedType, this.selectedBranch).toPromise();
+    this.series_Popup_List = await this.getData.get_Description_F(this.StartDate, this.EndDate, event.name, this.selectedUnit, this.selectedType, this.selectedBranch).toPromise();
     this.flag_loading = false;
     if (this.series_Popup_List.length > 0){
       this.flag_popup = true;
@@ -281,7 +339,10 @@ export class IncUnitComponent implements OnInit {
     this.dateform.controls['EndDate'].setValue(this.EndDate);
     this.flag_time = true;
     await this.do_fillter();
-    await this.GetData(this.StartDate, this.EndDate, this.selectedUnit, this.selectedType, this.selectedBranch);
+    if(!this.flag_toggle_filter)
+      await this.GetData(this.StartDate,this.EndDate);
+    else
+      await this.GetData_F(this.StartDate,this.EndDate,this.selectedUnit, this.selectedType, this.selectedBranch);
   }
 
   async SetTime(days:number){
@@ -293,7 +354,10 @@ export class IncUnitComponent implements OnInit {
     this.dateform.controls['EndDate'].setValue(this.EndDate);
     this.flag_time = true;
     await this.do_fillter();
-    await this.GetData(this.StartDate,this.EndDate,this.selectedUnit, this.selectedType, this.selectedBranch);
+    if(!this.flag_toggle_filter)
+      await this.GetData(this.StartDate,this.EndDate);
+    else
+      await this.GetData_F(this.StartDate,this.EndDate,this.selectedUnit, this.selectedType, this.selectedBranch);
   }
 
   async do_fillter(){
@@ -323,14 +387,13 @@ export class IncUnitComponent implements OnInit {
       section.nativeElement.scrollIntoView({ behavior: 'smooth' });
     }
   }
-
-  async onTypeChange() {
-  }
-  async onUnitChange() {
-  }
-  async onBranchChange() {
-  }
   async onSearchClicked() {
-    await this.GetData(this.StartDate,this.EndDate,this.selectedUnit, this.selectedType, this.selectedBranch);
+    if(!this.flag_toggle_filter)
+      this.flag_toggle_filter = true;
+    await this.GetData_F(this.StartDate,this.EndDate,this.selectedUnit, this.selectedType, this.selectedBranch);
+  }
+
+  async check_filter(){
+    this.flag_toggle_filter = !(!this.flag_filter_type && !this.flag_filter_unit && !this.flag_filter_branch);
   }
 }
--- src/app/services/inc-unit.service.ts
+++ src/app/services/inc-unit.service.ts
@@ -22,23 +22,43 @@ export class IncUnitService {
     return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetAllBranchs?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
   }
 
-  get_CountDay(StartDate:string, EndDate:string, Unit:string, Type:string, Branch:string):Observable<any>{
-    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetCountDay?startDate=${StartDate}&endDate=${EndDate}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  get_CountDay(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetCountDay?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
   }
 
-  get_TopReasons(StartDate:string, EndDate:string, Unit:string, Type:string, Branch:string):Observable<any>{
-    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetTopReasons?startDate=${StartDate}&endDate=${EndDate}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  get_TopReasons(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetTopReasons?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
   }
 
-  get_PhonecallReasons(StartDate:string, EndDate:string, Unit:string, Type:string, Branch:string):Observable<any>{
-    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetPhonecallReasons?startDate=${StartDate}&endDate=${EndDate}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  get_PhonecallReasons(StartDate:string, EndDate:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetPhonecallReasons?startDate=${StartDate}&endDate=${EndDate}`,{headers:this.header});
   }
 
-  get_ReasonDetail(StartDate:string, EndDate:string, Reason:string, Unit:string, Type:string, Branch:string):Observable<any>{
-    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetReasonDetail?startDate=${StartDate}&endDate=${EndDate}&Reason=${Reason}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  get_ReasonDetail(StartDate:string, EndDate:string, Reason:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetReasonDetail?startDate=${StartDate}&endDate=${EndDate}&Reason=${Reason}`,{headers:this.header});
   }
 
-  get_Description(StartDate:string, EndDate:string, Detail:string, Unit:string, Type:string, Branch:string):Observable<any>{
-    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetDescription?startDate=${StartDate}&endDate=${EndDate}&Detail=${Detail}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  get_Description(StartDate:string, EndDate:string, Detail:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetDescription?startDate=${StartDate}&endDate=${EndDate}&Detail=${Detail}`,{headers:this.header});
+  }
+
+  get_CountDay_F(StartDate:string, EndDate:string, Unit:string, Type:string, Branch:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetCountDay_F?startDate=${StartDate}&endDate=${EndDate}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  }
+
+  get_TopReasons_F(StartDate:string, EndDate:string, Unit:string, Type:string, Branch:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetTopReasons_F?startDate=${StartDate}&endDate=${EndDate}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  }
+
+  get_PhonecallReasons_F(StartDate:string, EndDate:string, Unit:string, Type:string, Branch:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetPhonecallReasons_F?startDate=${StartDate}&endDate=${EndDate}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  }
+
+  get_ReasonDetail_F(StartDate:string, EndDate:string, Reason:string, Unit:string, Type:string, Branch:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetReasonDetail_F?startDate=${StartDate}&endDate=${EndDate}&Reason=${Reason}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
+  }
+
+  get_Description_F(StartDate:string, EndDate:string, Detail:string, Unit:string, Type:string, Branch:string):Observable<any>{
+    return this.http.get(`${this.appConfigService.getApiUrl()}InComingCall_Unit/GetDescription_F?startDate=${StartDate}&endDate=${EndDate}&Detail=${Detail}&unit=${Unit}&type=${Type}&branch=${Branch}`,{headers:this.header});
   }
 }
